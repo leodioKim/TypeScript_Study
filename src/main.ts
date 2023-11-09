1ee016ba138355aca470e112a7f2384002fff6b3
leodioKim <kevin8603k@gmail.com>
2023-11-09 20:56:02 +0900

    second

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,88 @@
-let hello: string = "Hello world";
+let hello1: string = "Hello world";
 
-hello = "Hello~";
+hello1 = "Hello~";
 
-console.log(hello);
+console.log(hello1);
+
+// 타입 종류
+
+//문자
+// let str: string;
+// let red: string = "Red";
+// let green: string = "Green";
+// let myColor: string = `My color is ${red}.`;
+// let yourColor: string = "Your color is" + green;
+
+//숫자
+// let num: number;
+// let integer: number = 6;
+// let float: number = 3.14;
+// let infinity: number = Infinity;
+// let nan: number = NaN;
+
+//불린
+// let isBoolean: boolean;
+// let isDone: boolean = false;
+
+//Null / Undefined
+// let nul: null;
+// let und: undefined;
+// nul = null;
+// let num: number;
+// num = 123;
+// console.log(nul);
+// console.log(und);
+// console.log(num);
+
+//배열
+// const fruits: string[] = ["Apple", "Banana", "Cherry"];
+// const numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
+// const union: (string | number)[] = ["Apple", 1, 2, "Banana", 3];
+// const array: number[] = [1, 2, 3];
+
+//객체
+// const obj: object = {};
+// const arr: object = [];
+// const func: object = function () {};
+
+// interface User {
+//   name: string;
+//   age: number;
+//   isValid: boolean;
+// }
+// const userA: User = {
+//   name: "Heropy",
+//   age: 85,
+//   isValid: true,
+// };
+// const userB: {
+//   name: string;
+//   age: number;
+//   isValid: boolean;
+// } = {
+//   name: "Neo",
+//   age: 25,
+//   isValid: true,
+// };
+
+//함수
+// const add: (x: number, y: number) => number = function (
+//   x: number,
+//   y: number
+// ): number {
+//   return x + y;
+// };
+// const a: number = add(1, 2);
+
+// const hello: () => void = function () {
+//   console.log("Hello world~");
+// };
+// const h: void = hello();
+
+//Any
+//Unknown
+//Tuple
+//Void
+//Never
+//Union
+//Intersection
